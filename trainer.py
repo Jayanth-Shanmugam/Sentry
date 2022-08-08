109d92179039d1c2c88a8c6bdf0d54548003cb40
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2022-08-08 19:22:47 +0530

    Trainer for data

---
+++ trainer.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+import os
+from PIL import Image
+
+if(os.path.exists("classifier")):
+    print("Directory Exists!")
+else:
+    os.mkdir("classifier")
+
+def train(face_detect, face_recognize):
+    paths = [os.path.join('dataset', fname) for fname in os.listdir('dataset')]
+    faces_tr = []
+    labels = []
+
+    for path in paths:
+        pil_img = Image.open(path).convert("L")
+        cv_img = np.array(pil_img, 'uint8')
+        id = int(path.split()[-1].split(".")[1])
+
+        faces = face_detect.detectMultiScale(cv_img, 1.3, 5)
+        for x,y,w,h in faces:
+            faces_tr.append(cv_img[y:y+h, x:x+w])
+            labels.append(id)
+
+    print("Training the model......\n")
+    face_recognize.train(faces_tr, np.array(labels))
+    print("Training complete!\n")
+    face_recognize.write('classifier/classifier.yml')
+
+
+
+
+
